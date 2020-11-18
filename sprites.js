15ac0ac8e7249014d0efbc200c1a53d930a5844a
Denilson V <deethedev@gmail.com>
2020-11-17 21:54:41 -0500

    Convert funcs to async

--- sprites.js
+++ sprites.js
@@ -1,12 +1,16 @@
 import SpriteSheet from "./SpriteSheet.js";
 import { loadImage } from "./loaders.js";
 
-export function loadSprites() {
-  return loadImage("mr.png")
-      .then((image) => {
-        const sprites = new SpriteSheet(image, 17, 36);
-        sprites.define("mr", 0, 0, 17, 36);
-        // sprites.define("sky", 0, 0, 17, 17);
-        return sprites;
-      });
+export async function loadSprites() {
+  const image = await loadImage("mr.png");
+  const sprites = new SpriteSheet(image, 17, 36);
+  sprites.define("mr", 0, 0, 17, 36);
+  return sprites;
+}
+export async function loadBackgroundSprites() {
+  const image = await loadImage("mr.png");
+  const sprites = new SpriteSheet(image, 16, 16);
+  sprites.define("brick", 118, 49, 16, 16);
+  sprites.define("ground", 166, 33, 16, 16);
+  return sprites;
 }
